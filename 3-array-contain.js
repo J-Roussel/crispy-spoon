cbf6f67585c9b13365bfc2795dcfa68e994c23a3
Roussel <98948957+J-Roussel@users.noreply.github.com>
2022-04-01 17:01:04 +0300

    feat : answer 3-array-contain.js

--- 3-array-contain.js
+++ 3-array-contain.js
@@ -1,10 +1,16 @@
 function arrayContain(array, number) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === number) {
-      return true;
+  for (let i = 0; i < array.length; i++) { //5 +N 
+    if (array[i] === number) { //3
+      return true; // N
     }
   }
-  return false;
+  return false; // N
 }
 
-module.exports = { arrayContain };
+module.exports = { arrayContain }; //N
+
+/*
+LINEAIRE
+T = O(n) = 5+N+3+N+N+N 
+T = O(n) = 7 + 4N
+*/
