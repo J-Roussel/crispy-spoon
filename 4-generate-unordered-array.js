b3cf8ac080c93f5be35f53a00b89cd5dfcb838f6
Roussel <98948957+J-Roussel@users.noreply.github.com>
2022-04-01 17:12:14 +0300

    feat : 4-generate-unordered

--- 4-generate-unordered-array.js
+++ 4-generate-unordered-array.js
@@ -1,15 +1,21 @@
-const { arrayContain } = require('./3-array-contain');
+const { arrayContain } = require('./3-array-contain'); // 1
 
 function generateUnorderedArray(size) {
-  let array = [];
-  for (let i = 0; i < size; i++) {
-    let number = Math.floor(Math.random() * 100);
-    while (arrayContain(array, number)) {
-      number = Math.floor(Math.random() * 100);
+  let array = []; //1
+  for (let i = 0; i < size; i++) { //5 + N
+    let number = Math.floor(Math.random() * 100);//5N
+    while (arrayContain(array, number)) {//1 + N
+      number = Math.floor(Math.random() * 100);//5N
     }
-    array.push(number);
+    array.push(number);//3N
   }
-  return array;
+  return array;//1
 }
 
-module.exports = { generateUnorderedArray };
+module.exports = { generateUnorderedArray };//1
+
+/*
+Quadratique
+T = O(n*n) = 1+1+5+N+5N+1+N+5N+3N+1+1
+T = O(n*n) = 10 + 15N
+*/
