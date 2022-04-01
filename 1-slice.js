8a91c727673e9ba83a7d2e6eb2bd16822bed2819
Roussel <98948957+J-Roussel@users.noreply.github.com>
2022-04-01 16:43:27 +0300

    feat : answer 1.slice.js

--- 1-slice.js
+++ 1-slice.js
@@ -1,9 +1,15 @@
 function slice(array, start, end) {
-  let slicedArray = [];
-  for (let i = start; i < end; i++) {
-    slicedArray.push(array[i]);
+  let slicedArray = [];  //1
+  for (let i = start; i < end; i++) { // N + 6
+    slicedArray.push(array[i]); // 3N
   }
-  return slicedArray;
+  return slicedArray; // 1
 }
 
-module.exports = { slice };
+module.exports = { slice }; // 1
+
+/* Linéaire donc :
+T = O(n) = 1 + N + 6 + 3N + 1 + 1 = 4N + 9
+*/
+
+
