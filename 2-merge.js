21654f74f03f3a2516777985ecb8886b52435e52
Roussel <98948957+J-Roussel@users.noreply.github.com>
2022-04-01 16:58:15 +0300

    feat : 2.merge.js

--- 2-merge.js
+++ 2-merge.js
@@ -1,25 +1,28 @@
 function merge(array1, array2) {
-  let mergedArray = [];
-  let i = 0;
-  let j = 0;
-  while (i < array1.length && j < array2.length) {
-    if (array1[i] < array2[j]) {
-      mergedArray.push(array1[i]);
-      i++;
+  let mergedArray = []; //1
+  let i = 0; //1
+  let j = 0; //1
+  while (i < array1.length && j < array2.length) { // 7 + N
+    if (array1[i] < array2[j]) { //3N
+      mergedArray.push(array1[i]); //3N
+      i++;//1N
     } else {
-      mergedArray.push(array2[j]);
-      j++;
+      mergedArray.push(array2[j]); // 3N
+      j++;//1N
     }
   }
-  while (i < array1.length) {
-    mergedArray.push(array1[i]);
-    i++;
+  while (i < array1.length) {// N + 3
+    mergedArray.push(array1[i]); //3N
+    i++;//1N
   }
-  while (j < array2.length) {
-    mergedArray.push(array2[j]);
-    j++;
+  while (j < array2.length) { //3 + N
+    mergedArray.push(array2[j]); //3N
+    j++;//1N
   }
-  return mergedArray;
+  return mergedArray; //1
 }
 
-module.exports = { merge };
+module.exports = { merge };//1
+
+T = 1+1+1+7+N+3N+3N+N+N+3+3N+N+3+N+3N+N+1+1
+T = 18 + 18N
